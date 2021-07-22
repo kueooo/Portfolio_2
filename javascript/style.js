1d2c59b05a19a2f9063a0149161d2a78aca7c735
Yuan Wu <wu.yuanf@gmail.com>
2021-07-22 19:09:08 -0400

    upload

---
+++ javascript/style.js
@@ -0,0 +1,12 @@
+if $(".hero").on ("click", function(){
+    $(".hero").css("transition", "box-shadow", ".3s","ease","0s");
+    //     transition: box-shadow 0.3s ease 0s, opacity 200ms ease 0s, transform 300ms ease 0s;
+    // opacity: 1;
+    // transform: translateX(0px) translateY(0px) translateZ(0px) scaleX(1) scaleY(1) scaleZ(1);
+    // display: block;
+    // transform-style: preserve-3d;
+else {
+    console.log("ready to call a funciton")
+}
+    
+})
